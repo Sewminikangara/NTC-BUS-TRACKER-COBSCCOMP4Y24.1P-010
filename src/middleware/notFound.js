acd6fe11f23820fa2d2481e4ede027269746e86e
K.M.S Sewmini Kangara <124496599+Sewminikangara@users.noreply.github.com>
2025-10-06 13:24:43 +0530

    fix the UTF-8 BOM and syntax errors in notFound.js

--- src/middleware/notFound.js
+++ src/middleware/notFound.js
@@ -1,8 +1,16 @@
 ﻿/**
+ * Not Found Middleware
+ * Handles 404 errors for undefined routes
+ */
 
 const { ApiError } = require('./errorHandler');
 
 /**
+ * Handle undefined routes and send 404 error
+ * @param {Object} req - Express request object
+ * @param {Object} res - Express response object
+ * @param {Function} next - Express next function
+ */
 const notFound = (req, res, next) => {
     const message = `Cannot find ${req.originalUrl} on this server!`;
     next(new ApiError(message, 404));
